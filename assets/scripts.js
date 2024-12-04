d68fb41f47216288316719e8f20e2791568a8c5b
Antoine Mistral <antoinemistral.dev@gmail.com>
2024-12-04 13:46:34 +0100

    Affichage dynamique des slides

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,3 +13,27 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carousel = document.querySelector('#carouselExampleIndicators');
+    
+    // Charger l'image du slide actif au chargement de la page
+    const initialActiveSlide = carousel.querySelector('.carousel-item.active img');
+    if (initialActiveSlide) {
+      initialActiveSlide.setAttribute('src', initialActiveSlide.dataset.src);
+      initialActiveSlide.setAttribute('srcset', initialActiveSlide.dataset.srcset);
+      initialActiveSlide.dataset.loaded = true;
+    }
+  
+    // Charger les images quand le slide change
+    carousel.addEventListener('slide.bs.carousel', function(event) {
+      const nextSlide = event.relatedTarget;
+      const img = nextSlide.querySelector('img');
+  
+      if (!img.dataset.loaded) {
+        img.setAttribute('src', img.dataset.src);
+        img.setAttribute('srcset', img.dataset.srcset);
+        img.dataset.loaded = true;
+      }
+    });
+  });
